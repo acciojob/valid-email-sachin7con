542e04b64d72ee27330353a87763894c138f9c8c
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-03-14 16:13:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function validEmail(str) {
   //your JS code here.
+	let flag = false; 
+	if(str.length<5){
+		flag = false;
+	}else if(str.charAt(0) === "@" || str.charAt(0) === "." || str.charAt(str.length-1) === "." || str.charAt(str.length-1) === "@"){
+		flag =  false;
+	}else if(str.includes("@") && str.includes(".")){
+		flag = true;
+	}else{
+		flag = false;
+	}
+	return flag;
 }
+
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
